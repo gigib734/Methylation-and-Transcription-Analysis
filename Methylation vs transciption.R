54a8de2a2816e8133700297aaceda2e98d676be1
gigib734 <gblo0002@student.monash.edu>
2026-06-04 14:37:52 +1000

    wq

--- Methylation vs transciption.R
+++ Methylation vs transciption.R
@@ -3,3 +3,18 @@ transcription_data <- read.csv("C:/Users/gbloc/OneDrive/Desktop/PCDHG_Gila/Codin
 Filtered_methylation_data <- methylation_data %>%
   select(CpG, Annotated_genes, chr_state, Effect_size)
 library(dplyr)
+library(tidyverse)
+library(readr)
+
+#Filtering of Methylation data to do it by gene
+
+str(Filtered_methylation_data)
+#separating annotated genes out
+Gene_Filtered_Methylation_data <- Filtered_methylation_data %>%
+  separate_rows(Annotated_genes, sep = ";") %>%
+  mutate(Annotated_genes = str_trim(Annotated_genes))
+
+
+#Filtering of Methylation data only using TSS regions
+
+
