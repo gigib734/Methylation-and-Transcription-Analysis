8c0a2df0eb62a4b64e607783839fc3e19199cc7d
gigib734 <gblo0002@student.monash.edu>
2026-06-04 14:14:03 +1000

    dwdq

---
+++ Methylation vs transciption.R
@@ -0,0 +1,5 @@
+methylation_data <- read.csv("C:/Users/gbloc/OneDrive/Desktop/PCDHG_Gila/Coding/Methylation & Transcription Correlation/Meta-analysis results DMPs blood PCDH genes - Meta-analysis results DMPs blood PCDH genes (1).csv")
+transcription_data <- read.csv("C:/Users/gbloc/OneDrive/Desktop/PCDHG_Gila/Coding/Methylation & Transcription Correlation/Meta-analysis results DEGs blood PCDH genes - Meta-analysis results DEGs blood PCDH genes.csv.csv")
+Filtered_methylation_data <- methylation_data %>%
+  select(CpG, Annotated_genes, chr_state, Effect_size)
+library(dplyr)
