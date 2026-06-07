6ee0e9bf624191ae15231c662883f81209d457f3
gigib734 <gblo0002@student.monash.edu>
2026-06-07 19:01:01 +1000

    g

--- Methylation vs transciption.R
+++ Methylation vs transciption.R
@@ -1,7 +1,7 @@
 methylation_data <- read.csv("C:/Users/gbloc/OneDrive/Desktop/PCDHG_Gila/Coding/Methylation & Transcription Correlation/Meta-analysis results DMPs blood PCDH genes - Meta-analysis results DMPs blood PCDH genes (1).csv")
 transcription_data <- read.csv("C:/Users/gbloc/OneDrive/Desktop/PCDHG_Gila/Coding/Methylation & Transcription Correlation/Meta-analysis results DEGs blood PCDH genes - Meta-analysis results DEGs blood PCDH genes.csv.csv")
 Filtered_methylation_data <- methylation_data %>%
-  select(CpG, Annotated_genes, chr_state, Effect_size)
+  select(CpG, Annotated_genes, chr_state, Effect_size, SE)
 library(dplyr)
 library(tidyverse)
 library(readr)
@@ -13,6 +13,18 @@ str(Filtered_methylation_data)
 Gene_Filtered_Methylation_data <- Filtered_methylation_data %>%
   separate_rows(Annotated_genes, sep = ";") %>%
   mutate(Annotated_genes = str_trim(Annotated_genes))
+
+#double check
+head(Gene_Filtered_Methylation_data)
+#meaneffectbygene
+Mean_gene_effect <- Gene_Filtered_Methylation_data %>%
+  group_by(Annotated_genes) %>%
+  summarise(
+    Gene = first(Annotated_genes),
+    N_CpGs = n(),
+    Mean_Effect_Size = mean(Effect_size, na.rm = TRUE),
+    
+  )
 
 
 #Filtering of Methylation data only using TSS regions
